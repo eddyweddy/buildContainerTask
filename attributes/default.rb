81869723f21f3cf2859ba5063cbc621715fd5d21
Edward Ng <eddyweddy@gmail.com>
2017-02-06 07:10:01 +1100

    1) added rvm, ruby and passenger to recipe 2) added submodule link to sinatra app

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,7 @@
 
+default['passenger-nginx']['ruby_version'] = '2.3.1'
+default['passenger-nginx']['passenger']['version'] = '5.0.30'
+default['passenger-nginx']['rvm']['rvm_shell'] = '/etc/profile.d/rvm.sh'
+default['passenger-nginx']['nginx']['user'] = 'www-data'
+
+default['nginx']['user'] = 'www-data'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,35 @@
-# empty default
+
+execute 'Install RVM gpg key' do
+  command 'curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -'
+  user 'root'
+  not_if 'which rvm'
+end
+
+execute 'Install RVM' do
+  command 'curl -sSL https://get.rvm.io | bash -s stable --ruby'
+  user 'root'
+  not_if 'which rvm'
+end
+
+execute 'add nginx user to rvm group' do
+  command "adduser #{node['passenger-nginx']['nginx']['user']} rvm"
+  user 'root'
+end
+
+bash 'installing ruby' do
+  code "source #{node['passenger-nginx']['rvm']['rvm_shell']} && rvm install #{node['passenger-nginx']['ruby_version']}"
+  user 'root'
+end
+
+bash 'setting installed ruby to default' do
+  code "source #{node['passenger-nginx']['rvm']['rvm_shell']} && rvm --default use #{node['passenger-nginx']['ruby_version']}"
+end
+
+bash 'installing passenger gem' do
+  code <<-EOF
+  source #{node['passenger-nginx']['rvm']['rvm_shell']}
+  gem install passenger -v #{node['passenger-nginx']['passenger']['version']}
+  EOF
+  user 'root'
+end
+
